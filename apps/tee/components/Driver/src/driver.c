ec7d633532d6d62ea69d9a9ef8ed447af8a31246
Hans Liljestrand <hans@liljestrand.dev>
2021-03-10 12:14:47 -0500

    First try at driver code

--- apps/tee/components/Driver/src/driver.c
+++ apps/tee/components/Driver/src/driver.c
@@ -9,14 +9,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #include <autoconf.h>
 #include <camkes.h>
 #include <stdio.h>
 
-#define CLIENT_BUF 20
-#define SERIAL_BUF (0x1000 - 8)
-#define MAGIC_NUMBER 01
+#include "driver.h"
 
 struct {
     uint32_t head;
@@ -29,29 +26,132 @@ void pre_init(void)
     set_putchar(interrupt_putchar_putchar);
 }
 
-int offload_sign(const char* fingerprint, char **signature)
+
+void send_message(char *msg, size_t len)
 {
-    printf("%s: Offloading attestation to hardware\n", get_instance_name());
-    printf("%c%s", MAGIC_NUMBER, fingerprint);
+    (void) len;
+    // FIXME: Needs to handle null characters!!!
+    printf("%c%s", MAGIC_NUMBER, msg);
+}
 
-    char buf[CLIENT_BUF];
-    while (1) {
-        seL4_Word badge = 0;
-        int i = 0;
-        do {
-            if (interrupt_getchar_buf->head == interrupt_getchar_buf->tail) {
-                seL4_Wait(interrupt_getchar_notification(), &badge);
+int wait_for_response(struct message *message)
+{
+    seL4_Word badge = 0;
+    int i = 0;
+    char next = 0;
+    int state = 0;
+    unsigned char length[2];
+
+    while (1)
+    {
+        if (interrupt_getchar_buf->head == interrupt_getchar_buf->tail)
+        {
+            seL4_Wait(interrupt_getchar_notification(), &badge);
+        }
+        next = interrupt_getchar_buf->buf[interrupt_getchar_buf->head];
+        printf("Got me a %c\n", next);
+
+        switch (state)
+        {
+        case 0:
+        {
+            printf("We're in state 0\n");
+            message->type[i] = next;
+
+            if (i == 1) 
+                state = 1;
+            break;
+        }
+        case 1:
+        {
+            printf("We're in state 1\n");
+            length[i - 2] = next;
+
+            if (i == 3) {
+                message->len = length[0] | length[1] << 8;
+                printf("Message size is %lu\n", message->len);
+                state = 2;
             }
-            buf[i] = interrupt_getchar_buf->buf[interrupt_getchar_buf->head];
-            interrupt_getchar_buf->head = (interrupt_getchar_buf->head + 1) % sizeof(interrupt_getchar_buf->buf);
-        } while (buf[i] != '\r' && ++i < CLIENT_BUF - 1);
+            break;
+        }
+        case 2:
+        {
+            printf("We're in state 2\n");
+            unsigned msg_pos = i - 4;
+            if (msg_pos >= MAX_MSG_SIZE) {
+                printf("Maximum message size exceeded (msg_pos is %u < %u)\n", msg_pos, MAX_MSG_SIZE);
+                state = -1;
+            }
 
-        buf[i] = 0;
-        printf("%s: got '%s'\n", get_instance_name(), buf);
+            message->msg[msg_pos] = next;
+            break;
 
-        *signature = malloc(strlen(buf) + 1);
-        strncpy(*signature, buf, strlen(buf) + 1);
-        (*signature)[strlen(buf)] = '\0';
-        return 0;
-    }
+        }
+        }
+
+        interrupt_getchar_buf->head = (interrupt_getchar_buf->head + 1) % sizeof(interrupt_getchar_buf->buf);
+        ++i;
+
+        if (state == 2 && (i - 4) == message->len)
+            return 0;
+        if (state < 0)
+            return state;
+    };
+}
+
+void setup_public_key(void)
+{
+    // SeL4 pk request flow
+    unsigned char pk_sel4[crypto_sign_PUBLICKEYBYTES];
+    unsigned char verified_public_key_sel4[crypto_sign_PUBLICKEYBYTES];
+
+    // Send request for key
+    unsigned char req[2] = RECEIVE_REQUEST_VERIFIED_PK_FPGA;
+    send_message(req, 2);
+
+    // Wait for message
+    struct message response;
+    wait_for_response(&response);
+
+    // TODO: Store, verify, or something...
+    return;
+}
+
+int offload_sign(const char *fingerprint, char **signature)
+{
+    printf("%s: Retrieving public key\n", get_instance_name());
+    setup_public_key();
+
+    printf("%s: Offloading attestation to hardware\n", get_instance_name());
+
+    // Construct request
+    u_int16_t length_int = 64;
+    unsigned char tag[2] = RECEIVE_MESSAGE_SIGNATURE_FPGA;
+    unsigned char length[2];
+    length[0] = length_int & 0xff;
+    length[1] = (length_int >> 8) & 0xff;
+    // example msg
+    unsigned char msg[64] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    // construct the TLV message
+    unsigned char message[4 + sizeof(msg)];
+    memcpy(message, tag, 2);
+    memcpy(&message[2], length, 2);
+    memcpy(&message[4], msg, (size_t)length_int);
+    send_message(message, 4 + sizeof(msg));
+
+    struct message response;
+    wait_for_response(&response);
+
+    *signature = malloc(response.len);
+    strncpy(*signature, response.msg, response.len);
+
+    printf("%s: got response\n", get_instance_name());
+    return 0;
 }
---
+++ apps/tee/components/Driver/src/driver.h
@@ -0,0 +1,44 @@
+#ifndef TEE_DRIVER_DRIVER_H
+#define TEE_DRIVER_DRIVER_H
+
+#include <camkes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <autoconf.h>
+#include <camkes.h>
+#include <stdio.h>
+
+#define CLIENT_BUF 20
+#define SERIAL_BUF (0x1000 - 8)
+
+#define MAX_MSG_SIZE 4096
+#define MAGIC_NUMBER 16
+
+#define RECEIVE_SECRET_KEY_FPGA "RK";
+#define RECEIVE_VERIFIED_PUBLIC_KEY_FPGA "RV";
+#define RECEIVE_MESSAGE_SIGNATURE_FPGA "RM";
+#define RECEIVE_SIGNED_MESSAGE_HOST "RS";
+#define RECEIVE_REQUEST_VERIFIED_PK_FPGA "RR";
+#define RECEIVE_VERIFIED_PUBLIC_KEY_HOST "RP";
+
+#define crypto_sign_SECRETKEYBYTES 32
+#define crypto_sign_PUBLICKEYBYTES 32
+
+unsigned char sk[crypto_sign_SECRETKEYBYTES];
+unsigned char pk[crypto_sign_PUBLICKEYBYTES];
+
+struct message {
+    char type[3];
+    size_t len;
+    char msg[MAX_MSG_SIZE];
+};
+
+void send_message(char *msg, size_t len);
+int request_verified_public_key(int fd);
+int receive_verified_pk(int fd, unsigned char *pk, unsigned char *verified_public_key);
+int wait_for_response(struct message *message);
+
+
+#endif //TEE_DRIVER_DRIVER_H
