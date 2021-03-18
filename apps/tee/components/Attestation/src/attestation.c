cb86c80844d0ade7632dc3c28fae32135e970114
Max Crone <max.crone@aalto.fi>
2021-03-18 14:43:17 +0200

    Calculate hash fingerprint of TA in Attestation

--- apps/tee/components/Attestation/src/attestation.c
+++ apps/tee/components/Attestation/src/attestation.c
@@ -7,6 +7,8 @@
 #include <camkes.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "tweetnacl.h"
 #include <buffer.h>
 
 char* attest_attest(const char *application)
@@ -14,7 +16,8 @@ char* attest_attest(const char *application)
     printf("%s: Taking measurements..\n", get_instance_name());
 
     // Test our access to the SharedData via dataport ta
-    char *ta_str = (char *) ta;
+    const char *ta_str = (const char *) ta;
+    /*
     printf("First %d bytes of TA (%p):", TA_BUFSIZE, &ta_str);
     for (int i = 0; i < TA_BUFSIZE; i++) {
         if (i % 16 == 0)
@@ -23,6 +26,15 @@ char* attest_attest(const char *application)
         ta_str++;
     }
     printf("\n");
+    */
+
+    // Calculate a hash over the code pages of the TA
+    unsigned char hash[64];
+    if (crypto_hash(hash, ta_str, TA_BUFSIZE)) {
+	printf("%s: Error while calculating hash\n", get_instance_name());
+    }
+
+    printf("%s: Hash = %lx\n", get_instance_name(), (unsigned long long) hash);
 
     printf("%s: call Driver\n", get_instance_name());
     char *out = NULL;
